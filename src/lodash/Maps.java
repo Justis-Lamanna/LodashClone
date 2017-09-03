e6b0c7b51f8355de0d4121dd793fc28ea12d9d4d
Justis Lamanna <jlamanna@murraystate.edu>
2017-09-03 07:54:27 -0500

    Has, Invert, InvertBy, Keys

--- src/lodash/Maps.java
+++ src/lodash/Maps.java
@@ -12,6 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 /**
@@ -229,6 +230,75 @@ public class Maps {
      * @return The value for the specified key, or default if not found.
      */
     public static <K, V> V get(Map<K, V> object, K key, V defaultValue){
+        Objects.requireNonNull(object);
         return object.getOrDefault(key, defaultValue);
+    }
+    
+    /**
+     * Get the value at the specified key.
+     * @param <K> The type of the key.
+     * @param <V> The type of the value.
+     * @param object The object to retrieve from.
+     * @param key The key to retrieve.
+     * @return The value for the specified key, or null if not found.
+     */
+    public static <K, V> V get(Map<K, V> object, K key){
+        Objects.requireNonNull(object);
+        return object.get(key);
+    }
+    
+    /**
+     * Test if the object has the specified key.
+     * @param <K> The key to check for.
+     * @param object The object to search.
+     * @param path The key to search for.
+     * @return True if the object has the key.
+     */
+    public static <K> boolean has(Map<K, ?> object, K path){
+        Objects.requireNonNull(object);
+        return object.containsKey(path);
+    }
+    
+    /**
+     * Creates a map where keys become values and vice versa.
+     * @param <K> The type of the key in the input object.
+     * @param <V> The type of the value in the input object.
+     * @param object The object to invert.
+     * @return The inverted object, with key/value pairs reversed.
+     */
+    public static <K, V> Map<V, K> invert(Map<K, V> object){
+        return invertBy(object, v -> v);
+    }
+    
+    /**
+     * Creates a map where keys become values, and values go through an iteratee to generate keys.
+     * @param <K> The type of the key in the input object.
+     * @param <V> The type of the value in the input object.
+     * @param <R> The type of the key in the output object.
+     * @param object The object to invert.
+     * @param iteratee The object to calculate keys from the object values.
+     * @return The inverted map.
+     */
+    public static <K, V, R> Map<R, K> invertBy(Map<K, V> object, Function<V, R> iteratee){
+        Objects.requireNonNull(object);
+        Objects.requireNonNull(iteratee);
+        Map<R, K> invert = new HashMap<>();
+        for(K key : object.keySet()){
+            R createdKey = iteratee.apply(object.get(key));
+            invert.put(createdKey, key);
+        }
+        return invert;
+    }
+    
+    /**
+     * Get a list of keys in the object.
+     * Iteration order is not guaranteed.
+     * @param <K> The type of the keys.
+     * @param object The object to get keys from.
+     * @return The keys of the object.
+     */
+    public static <K> List<K> keys(Map<K, ?> object){
+        Objects.requireNonNull(object);
+        return new ArrayList<>(object.keySet());
     }
 }   
